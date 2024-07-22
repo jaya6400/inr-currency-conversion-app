a96947ffd0fe4841680b785298d54c613128ee72
Jaya Dubey <jayadubey6402@gmail.com>
2024-07-22 16:33:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import './App.css';
-import CurrencyInput from "./currencyInput.js";
+import CurrencyInput from "./currencyInput";
 import {useState, useEffect} from "react";
 import axios from "axios";
 
 function App() {
-
   const [amount1, setAmount1] = useState(1);
   const [amount2, setAmount2] = useState(1);
   const [currency1, setCurrency1] = useState('USD');
-  const [currency2, setCurrency2] = useState('INR');
+  const [currency2, setCurrency2] = useState('EUR');
   const [rates, setRates] = useState([]);
 
 //to extract import export currency data from icegate portal used parsehub api
 //(but was unable to receive a response due to network error)
-//https://parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data
-//so I used a third-party api(fixer) to fetch current currency exchange data
-  useEffect(() => {
-    axios.get('https://api.apilayer.com/fixer/latest?base=USD&apikey=8El09v1tgPaDSKNR0TGCUrzqXBE6AdDI')
-      .then(response => {
-        console.log(response);
-        setRates(response.data.rates);
-      })
-  }, []);
+//https://parsehub.com/api/v2/projects/{tnShPoS2TVXt}/last_ready_run/data
+//so I used a third-party exchange rate(open-er)api to fetch current currency exchange data
 
+  useEffect(() => {
+    axios.get('https://open.er-api.com/v6/latest/USD')
+      .then(response => {
+        setRates(response.data.rates);
+      })  
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+  
   useEffect(() => {
     if (!!rates) {
-      function init() {
-        handleAmount1Change(1);
+      function init(){
+        handleAmount1Change(1); 
       }
       init();
-    }
-  }, [rates]); // eslint-disable-line react-hooks/exhaustive-deps
-  
+    }  // eslint-disable-next-line 
+  }, [rates]); 
+
   function format(number) {
     return number.toFixed(4);
-  }
-
+  }  
+ 
   function handleAmount1Change(amount1) {
     setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
     setAmount1(amount1);
@@ -59,7 +61,7 @@ function App() {
 
   return (
     <div>
-      <h2>Check the Current Currency Rate in INR</h2>
+      <h2>Check the Current INR Currency Conversion</h2>
       <CurrencyInput
         onAmountChange={handleAmount1Change}
         onCurrencyChange={handleCurrency1Change}
