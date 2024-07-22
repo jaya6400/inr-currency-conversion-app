7ccb8567a6a5960fdecd5a9480e2f7b058d457cc
Jaya Dubey <jayadubey6402@gmail.com>
2024-07-22 15:43:57 +0530

    adding initial files to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import CurrencyInput from "./currencyInput.js";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+  const [currency1, setCurrency1] = useState('USD');
+  const [currency2, setCurrency2] = useState('INR');
+  const [rates, setRates] = useState([]);
+
+//to extract import export currency data from icegate portal used parsehub api
+//(but was unable to receive a response due to network error)
+//https://parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data
+//so I used a third-party api(fixer) to fetch current currency exchange data
+  useEffect(() => {
+    axios.get('https://api.apilayer.com/fixer/latest?base=USD&apikey=8El09v1tgPaDSKNR0TGCUrzqXBE6AdDI')
+      .then(response => {
+        console.log(response);
+        setRates(response.data.rates);
+      })
+  }, []);
+
+  useEffect(() => {
+    if (!!rates) {
+      function init() {
+        handleAmount1Change(1);
+      }
+      init();
+    }
+  }, [rates]); // eslint-disable-line react-hooks/exhaustive-deps
+  
+  function format(number) {
+    return number.toFixed(4);
+  }
+
+  function handleAmount1Change(amount1) {
+    setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+    setAmount1(amount1);
+  }
+
+  function handleCurrency1Change(currency1) {
+    setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+    setCurrency1(currency1);
+  }
+
+  function handleAmount2Change(amount2) {
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+    setAmount2(amount2);
+  }
+
+  function handleCurrency2Change(currency2) {
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+    setCurrency2(currency2);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Check the Current Currency Rate in INR</h2>
+      <CurrencyInput
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
+        currencies={Object.keys(rates)}
+        amount={amount1}
+        currency={currency1} />
+      <CurrencyInput
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change}
+        currencies={Object.keys(rates)}
+        amount={amount2}
+        currency={currency2} />
     </div>
   );
 }
