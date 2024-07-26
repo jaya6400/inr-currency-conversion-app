58994abf2c8280238a1373ee1b5002e05df2c986
Jaya Dubey <jayadubey6402@gmail.com>
2024-07-27 00:11:07 +0530

    Create icegate-scraper.js

---
+++ src/icegate-scraper.js
@@ -0,0 +1,21 @@
+ // Import the necessary libraries
+ const axios = require('axios');
+ const cheerio = require('cheerio');
+ 
+ // The web scraping function
+ async function scrapeWebPage(url) {
+     // Fetch the webpage
+     const response = await axios.get(url);
+     // Parse the HTML content of the webpage
+     const $ = cheerio.load(response.data);
+     console.log(response);
+     // Use Cheerio to extract the required data
+     // Here's an example to scrape paragraph text
+     $('p').each((i, element) => {
+         //console.log($(element).text());
+     });
+ }
+ 
+ // Run the function with the URL of the webpage you want to scrape
+ scrapeWebPage('https://foservices.icegate.gov.in/#/services/viewExchangeRate');
+ 
