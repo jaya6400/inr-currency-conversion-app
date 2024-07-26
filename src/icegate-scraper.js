f9b86e4f219bc9c0531313109008119662680862
Jaya Dubey <jayadubey6402@gmail.com>
2024-07-27 01:35:43 +0530

    Revert "Update icegate-scraper.js"
    This reverts commit 4dc5ddfcc98850f855b379a3a5249028f2667c6a.

--- src/icegate-scraper.js
+++ src/icegate-scraper.js
@@ -1,35 +1,21 @@
-const ScrapingAntClient = require('@scrapingant/scrapingant-client');
-
-const client = new ScrapingAntClient({ apiKey: '64993754b26f40159d3b5e5d9ea297ad' });
-
-// Scrape the example.com site.
-client.scrape('https://foservices.icegate.gov.in/#/services/viewExchangeRate')
-    .then(res => console.log(res))
-    .catch(err => console.error(err.message));
-
-
-
-
-
-
-// Import the necessary libraries
-// const axios = require('axios');
- //const cheerio = require('cheerio');
+ // Import the necessary libraries
+ const axios = require('axios');
+ const cheerio = require('cheerio');
  
  // The web scraping function
-// async function scrapeWebPage(url) {
+ async function scrapeWebPage(url) {
      // Fetch the webpage
-    // const response = await axios.get(url);
+     const response = await axios.get(url);
      // Parse the HTML content of the webpage
-//     const $ = cheerio.load(response.data);
-     
+     const $ = cheerio.load(response.data);
+     console.log(response);
      // Use Cheerio to extract the required data
      // Here's an example to scrape paragraph text
-  //   $('p').each((i, element) => {
-  //       console.log($(element).text());
-   //  });
-// }
+     $('p').each((i, element) => {
+         //console.log($(element).text());
+     });
+ }
  
  // Run the function with the URL of the webpage you want to scrape
- //scrapeWebPage('https://foservices.icegate.gov.in/#/services/viewExchangeRate');
+ scrapeWebPage('https://foservices.icegate.gov.in/#/services/viewExchangeRate');
  
